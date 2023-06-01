1ffab01e318b2cdaf81e291ca2d105c56c7fe665
Viktor <131308799+oskypro@users.noreply.github.com>
2023-06-01 20:06:42 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,10 +1,3 @@
 def func1(s: str):
-    ''' func1 doc1  '''
-    ''' func2 doc2  '''
+    ''' func1 docstring1  '''
     return s.upper()
-
-
-
-def func1(s: str):
-    ''' func1 docstring '''
-    return s.capitalize()
